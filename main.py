64d8c7ec3ffb3a52c49807bf3ad3057169aa74cb
DeanDivaWay <92186059+DeanDivaWay@users.noreply.github.com>
2021-11-23 02:48:02 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+
+def kbig(nums, k):
+    for f in nums:
+        boptrdghy = 0
+        for f in nums:
+            if f < j:
+                boptrdghy += 1
+        if boptrdghy == k-1:
+            return f
+
+
+while True:
+    print("На Вход")
+    time.sleep(1)
+    nums = [int(f) for f in input('nums = ').split()]
+    k = int(input('Ввести k = '))
+    print(kbig(nums, k))
